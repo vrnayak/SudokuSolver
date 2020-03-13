76eb7966364bb31e4c3cc770c00c98b1d27301ce
vrnayak <vrnayak@umich.edu>
2020-03-13 16:25:57 -0400

    Implemented numEmptyCells()

--- Grid.cpp
+++ Grid.cpp
@@ -45,18 +45,26 @@ int Grid::getCol(int index) const { return index % ROW_SIZE; }
 // EFFECTS: Returns box of cell with given index
 int Grid::getBox(int index) const {
     
-	if (index % 9 == 0 || index % 9 == 1 || index % 9 == 2)
+	if (index % ROW_SIZE == 0 ||
+		index % ROW_SIZE == 1 ||
+		index % ROW_SIZE == 2)
 		return 0 + 3 * (index / 27);
-	else if (index % 9 == 3 || index % 9 == 4 || index % 9 == 5)
+	
+	else if (index % ROW_SIZE == 3 ||
+			 index % ROW_SIZE == 4 ||
+			 index % ROW_SIZE == 5)
 		return 1 + 3 * (index / 27);
-	else if (index % 9 == 6 || index % 9 == 7 || index % 9 == 8)
+	
+	else if (index % ROW_SIZE == 6 ||
+			 index % ROW_SIZE == 7 ||
+			 index % ROW_SIZE == 8)
 		return 2 + 3 * (index / 27);
 	else
 		return -1;
 } // getBox()
 
 // EFFECTS: Returns index of cell at given row and column
-int Grid::getIndex(int row, int col) const { return (9 * row + col); }
+int Grid::getIndex(int row, int col) const { return (ROW_SIZE * row + col); }
 
 // EFFECTS: Fills in the cell at given row & column with given value
 void Grid::set(int row, int col, int value) { grid[row][col] = value; }
@@ -93,6 +101,22 @@ int Grid::findFirstEmptyCell() const {
     } // for...row
     return -1;
 } // findFirstEmptyCell()
+
+// EFFECTS: Returns the number of empty cells
+int Grid::numEmptyCells() const {
+	
+	int count = 0;
+	for (int row = 0; row < ROW_SIZE; ++row) {
+		for (int col = 0; col < COL_SIZE; ++col) {
+			if (at(row, col) == 0)
+				count++;
+		} // for...col
+	} // for...row
+	return count;
+} // numEmptyCells()
+
+// EFFECTS: Returns whether the grid is filled or not
+bool Grid::isFilled() const { return (findFirstEmptyCell() == -1); }
 
 // EFFECTS: Prints sudoku grid to &os
 void Grid::print(std::ostream &os) const {
