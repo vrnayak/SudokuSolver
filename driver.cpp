d8c412cb9907a60798a215604973eb4dd6e1598e
vrnayak <vrnayak@umich.edu>
2020-03-13 16:26:32 -0400

    Fixed testing framework

--- driver.cpp
+++ driver.cpp
@@ -5,6 +5,7 @@
 //         Description: Reads in Sudoku Grid
 //
 
+#include <ctime>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -14,43 +15,75 @@
 
 using namespace std;
 
-const string PATHNAME = "/Users/vrnayak/Desktop/Coding/Personal/Sudoku Solver/Sudoku Grids/";
+const string IN = ".in";
+const string OUT = ".out";
+const string CORRECT = ".correct";
+const string PATHNAME = "/Users/vrnayak/Desktop/Coding/Personal/Sudoku Solver/Sudoku Grids/grid";
 
+const int NUM_GRIDS = 2;
+
+void printInfo();
 bool fileComp(string fileName1, string fileName2);
+bool ASSERT_TRUE(bool statement) {
+	
+	if (statement) return true;
+	else exit(3);
+}
 
 int main() {
 	
-	ifstream gridIn(PATHNAME + "grid2.in");
-	ofstream gridOut(PATHNAME + "grid2.out");
-	Grid sudokuGrid(gridIn);
-	
-	bruteForceSolve(sudokuGrid);
-	sudokuGrid.print(gridOut);
-	// if (fileComp(PATHNAME + "grid1.out",
-	// 			 PATHNAME + "grid1.correct"))
-	// 	cout << "Success!" << endl;
-	
+	printInfo();
+	for (int i = 1; i <= NUM_GRIDS; ++i) {
+		
+		string fileIn = PATHNAME + to_string(i) + IN;
+		string fileOut = PATHNAME + to_string(i) + OUT;
+		string fileCorrect = PATHNAME + to_string(i) + CORRECT;
+		
+		ifstream inputStream(fileIn);
+		ofstream outputStream(fileOut);
+		
+		Grid sudokuGrid(inputStream);
+		
+		cout << "Sudoku Grid #" << i << "\n"
+			 << "# of Empty Cells: "
+			 << sudokuGrid.numEmptyCells() << "\n";
+		
+		clock_t start;
+		double time;
+		
+		start = std::clock();
+		bruteForceSolve(sudokuGrid);
+		time = (std::clock() - start) / (double) CLOCKS_PER_SEC;
+		sudokuGrid.print(outputStream);
+		ASSERT_TRUE(fileComp(fileOut, fileCorrect));
+		cout << "Time taken to solve: " << time << "s\n";
+		cout << endl;
+	}
 	return 0;
-	
 } // main()
+
+void printInfo() {
+	
+	cout << "Sudoku Solver Program\n"
+		 << "# of Grids to Solve: " << NUM_GRIDS
+		 << endl << endl;
+}
 
 bool fileComp(string fileName1, string fileName2) {
 	
-	ifstream file1(fileName1);
-	ifstream file2(fileName2);
+	ifstream file1(fileName1), file2(fileName2);
 	
 	int cell1, cell2;
 	vector<int> grid;
 	grid.reserve(81);
 	
-	while (file1 >> cell1) {
+	while (file1 >> cell1)
 		grid.push_back(cell1);
-	} // while
-	
+
 	int counter = 0;
 	while (file2 >> cell2) {
 		if (grid[counter++] != cell2)
 			return false;
 	} // while
-	return true;
+	return counter == 81;
 }
