d88af584bbe7b0e087cc208f1f862dde0bc644dc
vrnayak <vrnayak@umich.edu>
2020-03-12 22:36:40 -0400

    Debugged bruteForceSolve()

--- solver.cpp
+++ solver.cpp
@@ -13,15 +13,19 @@ using namespace std;
 // EFFECTS: Performs brute force solving algorithm on sudoku grid
 void bruteForceSolve(Grid &grid) {
 	
+	int cell = grid.findFirstEmptyCell();
+	if (cell == -1) return;
+	
 	for (int i = 1; i <= 9; ++i) {
-		int cell = grid.findFirstEmptyCell();
-		if (cell == -1) break;
 		
 		if (grid.checkIfValid(grid.getRow(cell), grid.getCol(cell), i)) {
 			grid.set(grid.getRow(cell), grid.getCol(cell), i);
 			bruteForceSolve(grid);
+			if (grid.findFirstEmptyCell() == -1) break;
 		} // if...else
 	} // for...i
+	if (grid.findFirstEmptyCell() != -1)
+		grid.set(grid.getRow(cell), grid.getCol(cell), 0);
 } // bruteForceSolve()
 
 // EFFECTS: Performs simple process of elimination algorithm to fill in cells
