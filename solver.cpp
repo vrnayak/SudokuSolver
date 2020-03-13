59a4fbb3467cdbdcb3717431168f3bf4e450b191
vrnayak <vrnayak@umich.edu>
2020-03-13 15:15:11 -0400

    Implemented simpleSolve()

--- solver.cpp
+++ solver.cpp
@@ -5,7 +5,9 @@
 //         Description: Sudoku Solver Algorithms Implementation
 //
 
+#include <algorithm>
 #include <iostream>
+#include <numeric>
 #include "solver.h"
 
 using namespace std;
@@ -29,13 +31,34 @@ void bruteForceSolve(Grid &grid) {
 } // bruteForceSolve()
 
 // EFFECTS: Performs simple process of elimination algorithm to fill in cells
-void simpleSolve(Grid &sudokuGrid) {
+void simpleSolve(Grid &grid) {
 	
-	
+	for (int index = 0; index < 81; ++index) {
+		
+		vector<int> nums(9);
+		iota(nums.begin(), nums.end(), 1);
+		
+		if (grid.at(grid.getRow(index), grid.getCol(index)) == 0) {
+			
+			for (int i = 1; i <= 9; ++i) {
+				if (!grid.checkIfValid(grid.getRow(index), grid.getCol(index), i))
+					nums[i] = 0;
+			} // for...i
+			
+			if (count(nums.begin(), nums.end(), 0) == 8) {
+				for (int j = 0; j < 9; ++j) {
+					if (nums[j] != 0) {
+						grid.set(grid.getRow(index), grid.getCol(index), nums[j]);
+						break;
+					} // if...else
+				} // for...j
+			} // if...else
+		} // if...else
+	} // for...index
 } // simpleSolve()
 
 // EFFECTS: Performs basic algorithm utilized by humans to solve sudokus
-void smartSolve(Grid &sudokuGrid) {
+void smartSolve(Grid &grid) {
 	
 } // smartSolve()
 
