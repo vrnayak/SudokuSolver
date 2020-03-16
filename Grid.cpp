09f60162bb04bb71491d6a4545f34e1ed8335398
vrnayak <vrnayak@umich.edu>
2020-03-15 21:19:54 -0400

    Replaced hardcoded integers with variables

--- Grid.cpp
+++ Grid.cpp
@@ -5,6 +5,7 @@
 //         Description: Grid Class Implementation
 //
 
+#include <cmath>
 #include <iostream>
 #include "Grid.h"
 
@@ -14,8 +15,8 @@ using namespace std;
 // EFFECTS: Initializes a 9x9 grid object and fills it with 0's
 Grid::Grid() {
     
-    for (int row = 0; row < ROW_SIZE; ++row) {
-        for (int col = 0; col < COL_SIZE; ++col) {
+    for (int row = 0; row < NUM_ROWS; ++row) {
+        for (int col = 0; col < NUM_COLS; ++col) {
             set(row, col, 0);
         } // for...col
     } // for...row
@@ -25,8 +26,8 @@ Grid::Grid() {
 Grid::Grid(istream &is) {
     
     int cellNum;
-    for (int row = 0; row < ROW_SIZE; ++row) {
-        for (int col = 0; col < COL_SIZE; ++col) {
+    for (int row = 0; row < NUM_ROWS; ++row) {
+        for (int col = 0; col < NUM_COLS; ++col) {
             is >> cellNum;
             set(row, col, cellNum);
         } // for...col
@@ -37,34 +38,34 @@ Grid::Grid(istream &is) {
 int Grid::at(int row, int col) const { return grid[row][col]; }
 
 // EFFECTS: Returns row of cell with given index
-int Grid::getRow(int index) const { return index / ROW_SIZE; }
+int Grid::getRow(int index) const { return index / NUM_COLS; }
 
 // EFFECTS: Returns column of cell with given index
-int Grid::getCol(int index) const { return index % ROW_SIZE; }
+int Grid::getCol(int index) const { return index % NUM_COLS; }
 
 // EFFECTS: Returns box of cell with given index
 int Grid::getBox(int index) const {
     
-	if (index % ROW_SIZE == 0 ||
-		index % ROW_SIZE == 1 ||
-		index % ROW_SIZE == 2)
+	if (index % NUM_COLS == 0 ||
+		index % NUM_COLS == 1 ||
+		index % NUM_COLS == 2)
 		return 0 + 3 * (index / 27);
 	
-	else if (index % ROW_SIZE == 3 ||
-			 index % ROW_SIZE == 4 ||
-			 index % ROW_SIZE == 5)
+	else if (index % NUM_COLS == 3 ||
+			 index % NUM_COLS == 4 ||
+			 index % NUM_COLS == 5)
 		return 1 + 3 * (index / 27);
 	
-	else if (index % ROW_SIZE == 6 ||
-			 index % ROW_SIZE == 7 ||
-			 index % ROW_SIZE == 8)
+	else if (index % NUM_COLS == 6 ||
+			 index % NUM_COLS == 7 ||
+			 index % NUM_COLS == 8)
 		return 2 + 3 * (index / 27);
 	else
 		return -1;
 } // getBox()
 
 // EFFECTS: Returns index of cell at given row and column
-int Grid::getIndex(int row, int col) const { return (ROW_SIZE * row + col); }
+int Grid::getIndex(int row, int col) const { return (NUM_COLS * row + col); }
 
 // EFFECTS: Fills in the cell at given row & column with given value
 void Grid::set(int row, int col, int value) { grid[row][col] = value; }
@@ -76,7 +77,7 @@ bool Grid::checkIfValid(int rowCheck, int colCheck, int value) {
     set(rowCheck, colCheck, value);
     int cellIndex = getIndex(rowCheck, colCheck);
     int boxStartIndex = 27 * (getBox(cellIndex) / 3) +
-						3 * (getBox(cellIndex) % 3);
+						 3 * (getBox(cellIndex) % 3);
     
     if (getCount(value, cellIndex, "row") > 1 ||
         getCount(value, cellIndex, "col") > 1 ||
@@ -93,8 +94,8 @@ bool Grid::checkIfValid(int rowCheck, int colCheck, int value) {
 // EFFECTS: Returns index of first empty cell in sudoku grid
 int Grid::findFirstEmptyCell() const {
     
-    for (int row = 0; row < ROW_SIZE; ++row) {
-        for (int col = 0; col < COL_SIZE; ++col) {
+    for (int row = 0; row < NUM_ROWS; ++row) {
+        for (int col = 0; col < NUM_COLS; ++col) {
             if (at(row, col) == 0)
 				return getIndex(row, col);
         } // for...col
@@ -106,8 +107,8 @@ int Grid::findFirstEmptyCell() const {
 int Grid::numEmptyCells() const {
 	
 	int count = 0;
-	for (int row = 0; row < ROW_SIZE; ++row) {
-		for (int col = 0; col < COL_SIZE; ++col) {
+	for (int row = 0; row < NUM_ROWS; ++row) {
+		for (int col = 0; col < NUM_COLS; ++col) {
 			if (at(row, col) == 0)
 				count++;
 		} // for...col
@@ -121,8 +122,8 @@ bool Grid::isFilled() const { return (findFirstEmptyCell() == -1); }
 // EFFECTS: Prints sudoku grid to &os
 void Grid::print(std::ostream &os) const {
     
-    for (int row = 0; row < ROW_SIZE; ++row) {
-		for (int col = 0; col < COL_SIZE; ++col) {
+    for (int row = 0; row < NUM_ROWS; ++row) {
+		for (int col = 0; col < NUM_COLS; ++col) {
 			os << at(row, col) << " ";
 		} // for...col
         os << endl;
@@ -136,20 +137,20 @@ int Grid::getCount(int value, int start, std::string regionType) const {
     
     int count = 0;
     if (regionType == "row") {
-        for (int col = 0; col < COL_SIZE; ++col) {
+        for (int col = 0; col < NUM_COLS; ++col) {
             if (at(getRow(start), col) == value)
 				count++;
         } // for...col
 		
     } else if (regionType == "col") {
-        for (int row = 0; row < ROW_SIZE; ++row) {
+        for (int row = 0; row < NUM_ROWS; ++row) {
             if (at(row, getCol(start)) == value)
 				count++;
         } // for...row
 		
     } else if (regionType == "box") {
-        for (int rowDiff = 0; rowDiff < 3; ++rowDiff) {
-            for (int colDiff = 0; colDiff < 3; ++colDiff) {
+        for (int rowDiff = 0; rowDiff < sqrt(NUM_ROWS); ++rowDiff) {
+            for (int colDiff = 0; colDiff < sqrt(NUM_COLS); ++colDiff) {
                 if (at(getRow(start) + rowDiff,
 					   getCol(start) + colDiff) == value)
                     count++;
